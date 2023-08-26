f77fa6ddee4d06bfa9da2f77b10a13697f16f3d5
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-08-26 12:43:48 +0200

    HW-03 made steps 1-4 with schema project

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
-const app = require('./app')
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+const connection = mongoose.connect(process.env.DB_CONTACTS_URL, {
+	dbName: 'db-contacts',
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+
+connection
+	.then(() => {
+		console.log('Database connection successful');
+		app.listen(PORT, () => {
+			console.log(`App listens on port ${PORT}`);
+		});
+	})
+	.catch(err => {
+		console.error(`Error while establishing connection: [${err}]`);
+		process.exit(1);
+	});
+
+// app.listen(3000, () => {
+// 	console.log('Server running. Use our API on port: 3000');
+// });
